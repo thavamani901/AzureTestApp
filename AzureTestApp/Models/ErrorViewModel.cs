4eba092288437070496cfad71afea745393a82e8
x x <thavamani.90@gmail.com>
2022-01-29 21:52:20 +0530

    testapp

---
+++ AzureTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
